cb50e75df72b004d3de9afc7a0e0db435d07a43e
Mo-Mans0ur <Moha1998@outlook.dk>
2022-02-15 18:24:53 +0100

    done

---
+++ src/main/java/com/example/bin2dec/Bin2DecApplication.java
@@ -0,0 +1,13 @@
+package com.example.bin2dec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bin2DecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bin2DecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bin2dec/Bin2DecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bin2dec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bin2DecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
